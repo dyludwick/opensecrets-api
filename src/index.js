c19e95d1760990e6464e5cb430fbd06903ad1eda
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-25 14:34:16 -0600

    check for api key

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import dotenv from 'dotenv';
 
 // config dotenv
 dotenv.config();
+
+// Check for api key
+const apikey = process.env.OPENSECRETS_API_KEY;
+!apikey ? console.log('Warning: OpenSecrets API key required') : console.log('api key found');
