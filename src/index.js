803e088419e252cb86da44839eab7ec49eebf98a
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-25 15:43:23 -0600

    install and config isomorphic fetch

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import 'babel-polyfill';
 import dotenv from 'dotenv';
+import promise from 'es6-promise';
+import 'isomorphic-fetch';
 
-// config dotenv
+// Config dotenv
 dotenv.config();
+
+// Init es-6 promise
+promise.polyfill();
 
 // Check for api key
 const apikey = process.env.OPENSECRETS_API_KEY;
 !apikey ? console.log('Warning: OpenSecrets API key required') : console.log('api key found');
 
+// Define api call class
 class OpenSecretsCall {
   constructor(method, params) {
     this.apikey = process.env.OPENSECRETS_API_KEY;
     this.output = 'json';
     this.method = method;
     this.params = params;
-    this.baseurl = 'http://http://www.opensecrets.org/api/';
+    this.baseurl = 'http://www.opensecrets.org/api/';
   }
 
   fetchData() {
+    // Set url
     let url = `${this.baseurl}?method=${this.method}&apikey=${this.apikey}&output=json&id=${this.params}`;
     console.log(url);
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 }
 
