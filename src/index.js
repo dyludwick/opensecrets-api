0af17655ba895c6ff786c7a49a7eb3b683728820
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-25 15:16:32 -0600

    create api call class

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ dotenv.config();
 // Check for api key
 const apikey = process.env.OPENSECRETS_API_KEY;
 !apikey ? console.log('Warning: OpenSecrets API key required') : console.log('api key found');
+
+class OpenSecretsCall {
+  constructor(method, params) {
+    this.apikey = process.env.OPENSECRETS_API_KEY;
+    this.output = 'json';
+    this.method = method;
+    this.params = params;
+    this.baseurl = 'http://http://www.opensecrets.org/api/';
+  }
+
+  fetchData() {
+    let url = `${this.baseurl}?method=${this.method}&apikey=${this.apikey}&output=json&id${this.params}`;
+    console.log(url);
+  }
+}
+
+const getLegislators = new OpenSecretsCall
