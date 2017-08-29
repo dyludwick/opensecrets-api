459ff179cedae14b9a27cd5aa13eebe1da0575f4
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-29 11:25:31 -0600

    add typeof asserts to check output test

--- test.js
+++ test.js
@@ -7,11 +7,14 @@ test('Check output returns correct output', async t => {
   const candSummaryXml = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, 'xml');
   const candSummaryErr = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, 'jso');
 
-  // Output defined as json when unspecified
+  // Output defined as 'json' when unspecified
+  t.is(typeof candSummary.checkOutput(), 'string');
   t.is(candSummary.checkOutput(), 'json');
-  // Output defined as json when specified as json
+  // Output defined as 'json' when specified as json
+  t.is(typeof candSummaryJson.checkOutput(), 'string');
   t.is(candSummaryJson.checkOutput(), 'json');
-  // Output defined as xml when specified as xml
+  // Output defined as 'xml' when specified as xml
+  t.is(typeof candSummaryXml.checkOutput(), 'string');
   t.is(candSummaryXml.checkOutput(), 'xml');
 
   // Throws error when output specified as invalid value
