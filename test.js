6e651499bc82578bef1a1f19b93021df686a9f9c
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-29 11:58:07 -0600

    write check api func tests

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 import test from 'ava';
 import OpenSecretsCall from './src/index';
 
-test('Check output returns correct output', async t => {
+// checkOutput() test [1]
+test('checkOutput returns correct output val', async t => {
   const candSummary = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'});
   const candSummaryJson = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, 'json');
   const candSummaryXml = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, 'xml');
@@ -23,4 +24,35 @@ test('Check output returns correct output', async t => {
   }, TypeError);
 
   t.is(error.message, 'Whoops! Output value is invalid');
+});
+
+// checkApiKey() tests [3]
+test('checkApiKey returns correct val when unspecified', async t => {
+  process.env.OPENSECRETS_API_KEY = 'apikey';
+  const candSummary = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'});
+
+  // Apikey defined as process.env.OPENSECRETS_API_KEY when unspecified
+  t.is(typeof candSummary.checkApiKey(), 'string');
+  t.is(candSummary.checkApiKey(), 'apikey');
+});
+
+test('checkApiKey returns correct val when specified', async t => {
+  process.env.OPENSECRETS_API_KEY = '';
+  const candSummary = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, '', 'apikey');
+
+  // Apikey defined as apikey param when specified
+  t.is(typeof candSummary.checkApiKey(), 'string');
+  t.is(candSummary.checkApiKey(), 'apikey');
+});
+
+test('checkApiKey throws correct err when apikey undefined', async t => {
+  process.env.OPENSECRETS_API_KEY = '';
+  const candSummary = new OpenSecretsCall('candSummary', { cid: 'N00007360', cycle: '2012'}, '', '');
+
+  // Throw err when apikey undefined
+  const error = await t.throws(() => {
+    candSummary.checkApiKey();
+  }, TypeError);
+
+  t.is(error.message, 'Whoops! OpenSecrets API key required');
 });
